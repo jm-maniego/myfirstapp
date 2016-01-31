c260d784be058cf9dc335e3ac5c5015e9ae58f05
jmmaniego <jmmaniego@hotmail.com>
2016-01-31 16:49:55 +0800

    precompile assets

---
+++ public/assets/resume-1a7db60403b593d819c8de723b6b8168b4e20ac3dfbf845fe11b5bca046a4a6a.js
@@ -0,0 +1,10 @@
+(function() {
+  $(function() {
+    return $("#nickname").typed({
+      strings: ["JM", "Migi", "Juan Miguel", "Ige"],
+      loop: true,
+      typeSpeed: 269
+    });
+  });
+
+}).call(this);
